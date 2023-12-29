ccfc83651719494b51b1dbabd17ea987ee3989e6
joeCodes05 <nathancodes05@gmail.com>
2023-12-29 15:36:25 +0100

    dummy text (Temprary)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <div className="mb-5 py-3 md:px-5 px-3"></div>;
+  return (
+    <div className="mb-5 py-3 md:px-5 px-3">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora debitis
+      non nobis. Voluptatum, ipsam esse libero excepturi provident at earum unde
+      amet nesciunt magni harum possimus deleniti fuga vitae perferendis? amet
+      nesciunt magni harum possimus deleniti fuga vitae perferendis? amet
+      nesciunt magni harum possimus deleniti fuga vitae perferendis? amet
+      nesciunt magni harum possimus deleniti fuga vitae perferendis? nesciunt
+      magni harum possimus deleniti fuga vitae perferendis? nesciunt magni harum
+      possimus deleniti fuga vitae perferendis? nesciunt magni harum possimus
+      deleniti fuga vitae perferendis? deleniti fuga vitae perferendis? deleniti
+      fuga vitae perferendis?
+    </div>
+  );
 };
 
 export default Home;
