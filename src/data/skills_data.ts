5fe4cd76288ddbd351d78c31865f565470f94107
joeCodes05 <nathancodes05@gmail.com>
2024-01-01 11:37:27 +0100

    file rename

---
+++ src/data/skills_data.ts
@@ -0,0 +1,33 @@
+import { SkillsObjectStructure } from "../types/object_types";
+
+export const skillsData: SkillsObjectStructure[] = [
+  {
+    title: "App development",
+    percentageValue: 60,
+  },
+
+  {
+    title: "Web development",
+    percentageValue: 95,
+  },
+
+  {
+    title: "UI/UX Design",
+    percentageValue: 50,
+  },
+
+  {
+    title: "Artificial Intelligence",
+    percentageValue: 80,
+  },
+
+  {
+    title: "Backend Development",
+    percentageValue: 80,
+  },
+
+  {
+    title: "Digital Marketing",
+    percentageValue: 50,
+  },
+];
