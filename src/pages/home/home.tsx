bab72b5a8c7f6a08a95d45ac7e022c9774176173
joeCodes05 <nathancodes05@gmail.com>
2024-01-01 09:40:58 +0100

    skills home-component init

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero";
 import CollaborationComponent from "./components/collaboration";
+import SkillsComponent from "./components/skills";
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
       <CollaborationComponent />
+      <SkillsComponent />
     </>
   );
 };
