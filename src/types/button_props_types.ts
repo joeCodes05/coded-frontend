673eb2a6e24b1d07bb2a9fb784fc32bc19f4b380
joeCodes05 <nathancodes05@gmail.com>
2024-01-01 08:12:00 +0100

    change on button  hover

--- src/types/button_props_types.ts
+++ src/types/button_props_types.ts
@@ -1,4 +1,12 @@
+import { IconType } from "react-icons";
+
 export interface ButtonPropsType {
   text: string;
   onClick: () => void;
+}
+
+export interface CollaborationType {
+  title: string;
+  content: string;
+  Icon: IconType;
 }
