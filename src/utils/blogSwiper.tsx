703b94b1f5431a709eb128953523b210f4c33c6f
joeCodes05 <nathancodes05@gmail.com>
2024-01-02 06:34:47 +0100

    reverted code

--- src/utils/blogSwiper.tsx
+++ src/utils/blogSwiper.tsx
@@ -38,49 +38,47 @@ const BlogSwiperUtil: React.FC = () => {
 
           return (
             <SwiperSlide key={index}>
-              <Link className="no-underline outline" to="/">
-                <div
-                  data-aos="fade-left"
-                  ata-aos-easing="ease-in"
-                  data-aos-duration="1000"
-                  className="group"
-                >
-                  <div className="h-[300px] overflow-hidden rounded-[16px] shadow relative group">
-                    <div className="absolute top-0 right-0 m-2 z-10 text-sm py-[2px] px-3 opacity-0 duration-300 group-hover:opacity-100 ease-in shadow rounded-md text-primary bg-black">
-                      {postCategory}
+              <div
+                data-aos="fade-left"
+                ata-aos-easing="ease-in"
+                data-aos-duration="1000"
+                className="group"
+              >
+                <div className="h-[300px] overflow-hidden rounded-[16px] shadow relative group">
+                  <div className="absolute top-0 right-0 m-2 z-10 text-sm py-[2px] px-3 opacity-0 duration-300 group-hover:opacity-100 ease-in shadow rounded-md text-primary bg-black">
+                    {postCategory}
+                  </div>
+                  <img
+                    src={postImage}
+                    alt={title}
+                    className="w-full h-full object-cover grayscale duration-300 group-hover:grayscale-0"
+                  />
+                </div>
+
+                <div className="lg:mt-8 mt-5 grid lg:grid-cols-2 grid-cols-1 place-content-center gap-2">
+                  <div>
+                    <h1 className="text-2xl">{title}</h1>
+                    <div className="mt-2 text-sm">
+                      by:{" "}
+                      <Link
+                        to="/"
+                        className="outline-none no-underline text-primary"
+                      >
+                        {postAuthor}
+                      </Link>
                     </div>
-                    <img
-                      src={postImage}
-                      alt={title}
-                      className="w-full h-full object-cover grayscale duration-300 group-hover:grayscale-0"
+                  </div>
+
+                  <div className="text-gray-400 font-light text-sm tracking-wide">
+                    <TextTruncate
+                      line={3}
+                      element="p"
+                      truncateText="..."
+                      text={content}
                     />
                   </div>
-
-                  <div className="lg:mt-8 mt-5 grid lg:grid-cols-2 grid-cols-1 place-content-center gap-2">
-                    <div>
-                      <h1 className="text-2xl">{title}</h1>
-                      <div className="mt-2 text-sm">
-                        by:{" "}
-                        <Link
-                          to="/"
-                          className="outline-none no-underline text-primary"
-                        >
-                          {postAuthor}
-                        </Link>
-                      </div>
-                    </div>
-
-                    <div className="text-gray-400 font-light text-sm tracking-wide">
-                      <TextTruncate
-                        line={3}
-                        element="p"
-                        truncateText="..."
-                        text={content}
-                      />
-                    </div>
-                  </div>
                 </div>
-              </Link>
+              </div>
             </SwiperSlide>
           );
         })}
