2ac758c558ca295f6a350ac87b646f82a967094d
joeCodes05 <nathancodes05@gmail.com>
2024-01-02 14:09:53 +0100

    minor code updates

--- src/utils/blogSwiper.tsx
+++ src/utils/blogSwiper.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 // import Swiper core and required modules
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-
+import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 import TextTruncate from "react-text-truncate";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css";
+import "swiper/css/navigation";
 import { blogPostData } from "../data/blog_data";
 import { Link } from "react-router-dom";
 
@@ -30,6 +32,10 @@ const BlogSwiperUtil: React.FC = () => {
       <Swiper
         modules={[Navigation]}
         grabCursor
+        navigation={{
+          nextEl: ".next-button",
+          prevEl: ".prev-button",
+        }}
         spaceBetween={20}
         slidesPerView={isMobile ? 1 : 2}
       >
@@ -87,6 +93,17 @@ const BlogSwiperUtil: React.FC = () => {
             </SwiperSlide>
           );
         })}
+
+        <div className="lg:mt-5 mt-0">
+          <div className="flex gap-4 items-center lg:p-5 p-10 w-fit ml-auto lg:mr-0 mr-auto ">
+            <button className="grid outline-none prev-button place-items-center text-primary h-[40px] w-[40px] ring-1 ring-primary rounded-full">
+              <IoIosArrowRoundBack />
+            </button>
+            <button className="grid next-button outline-none place-items-center text-primary h-[40px] w-[40px] ring-1 ring-primary rounded-full">
+              <IoIosArrowRoundForward />
+            </button>
+          </div>
+        </div>
       </Swiper>
     </>
   );
